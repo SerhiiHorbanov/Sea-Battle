98759ae3117eb293c2461bb7700db79609e17e9c
Serhii <serikgorbanev@gmail.com>
2023-04-12 18:24:58 +0200

    Splitted Enums into different .cs files because kikril said it's bad

---
+++ Sea Battle/GameEndResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Sea_Battle
+{
+    enum GameEndResult
+    {
+        P1Win,
+        P2Win,
+        Draw
+    }
+}
---
+++ Sea Battle/GameplayState.cs
@@ -0,0 +1,10 @@
+﻿namespace Sea_Battle
+{
+    enum GameplayState
+    {
+        P1PlacingShips,
+        P2PlacingShips,
+        P1Move,
+        P2Move,
+    }
+}
